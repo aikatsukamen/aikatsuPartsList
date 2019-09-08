16b02bf20c30b08449eb6625a3af2826e59ec29d
aikatsukamen <aikatsukamen@gmail.com>
2019-09-09 05:34:20 +0900

    ホームに登録できるようになったハズ

--- src/gui/sagas/index.ts
+++ src/gui/sagas/index.ts
@@ -8,6 +8,7 @@ import { Parts } from '../../types/entity';
 export default function* rootSaga() {
   // yield takeEvery(getType(actions.changeNotify), fetchCordiByAllRanking);
   yield call(fetchListAndApplyState);
+  yield call(createManifest);
 }
 
 function* fetchListAndApplyState() {
@@ -48,7 +49,6 @@ const fetchList = async () => {
   }
 };
 
-// サークル配置をGETする
 function fetchListFromGAS(qr?: string): Promise<{ data: Parts[]; error?: any }> {
   return new Promise((resolve, reject) => {
     let url = `https://script.google.com/macros/s/AKfycbxqkW2JY0dbmlikokIUnN59UMXqR2ICh0Z9ApVe8YmFPxHPrNA/exec`;
@@ -67,3 +67,40 @@ function fetchListFromGAS(qr?: string): Promise<{ data: Parts[]; error?: any }> 
       });
   });
 }
+
+const createManifest = () => {
+  const url = window.location.href;
+
+  const manifest = {
+    name: 'アイカツ！パーツリスト',
+    short_name: 'パーツリスト',
+    theme_color: '#2196f3',
+    background_color: '#2196f3',
+    display: 'standalone',
+    start_url: url,
+    icons: [
+      {
+        src: 'icon.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+    ],
+  };
+
+  const stringManifest = JSON.stringify(manifest);
+  const blob = new Blob([stringManifest], { type: 'application/json' });
+  const manifestURL = URL.createObjectURL(blob);
+  (document.querySelector('#my-manifest') as any).setAttribute('href', manifestURL);
+
+  // Service Worker登録
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('sw.js')
+      .then(registration => {
+        console.log('serviceWorker registed.');
+      })
+      .catch(error => {
+        console.warn('serviceWorker error.', error);
+      });
+  }
+};
